290b1d34c1a49b258b1c26cb72a205a2368bf697
blaine614 <blaine614@gmail.com>
2018-08-30 11:58:02 -0400

    Bootstrapped in recipe data. Implemented a Recipe Service. Updated some of the domains with fixes. Updated the Index Controller. Finally displayed bootstrap data on root path.

---
+++ src/main/java/guru/springframework/RecipeBootstrap.java
@@ -0,0 +1,125 @@
+package guru.springframework;
+
+import guru.springframework.domain.*;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        recipeRepository.saveAll(getRecipes());
+    }
+
+    private List<Recipe> getRecipes() {
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        //get UOMs
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+
+        if(!eachUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> tableSpoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        if(!tableSpoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> teaSpoonOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        if(!teaSpoonOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+
+        if(!dashUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+
+        if(!pintUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> cupUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        if(!cupUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tableSPoonUom = tableSpoonUomOptional.get();
+        UnitOfMeasure teaspoonUom = teaSpoonOptional.get();
+        UnitOfMeasure dashUom = dashUomOptional.get();
+        UnitOfMeasure pintUom = pintUomOptional.get();
+        UnitOfMeasure cupUom = cupUomOptional.get();
+
+        //get Categories
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+
+        if(!americanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+
+        if(!mexicanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found");
+        }
+
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacomole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setDirections("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " + "\n" +
+                "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation " + "\n" +
+                "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " + "\n" +
+                "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " + "\n" +
+                "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        );
+
+        Notes guacNotes = new Notes();
+        guacNotes.setRecipeNotes("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        guacNotes.setRecipe(guacRecipe);
+        guacRecipe.setNotes(guacNotes);
+
+        guacRecipe.getIngredients().add(new Ingredient("ripe avocados", new BigDecimal(2), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Kosher salt", new BigDecimal(.5), teaspoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("fresh lime juice", new BigDecimal(2), tableSPoonUom, guacRecipe));
+
+        guacRecipe.getCategories().add(americanCategory);
+        guacRecipe.getCategories().add(mexicanCategory);
+
+        recipes.add(guacRecipe);
+
+        return recipes;
+    }
+}
