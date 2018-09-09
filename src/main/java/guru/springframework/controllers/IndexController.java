e64a5320981b978ca1e189bcc76da9456298ea14
Blaine Morbitzer <blaine614@gmail.com>
2018-09-09 19:47:40 -0400

    Added in a bunch of testing. Unit Testing, Integration Testing, and CI.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,18 +1,11 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.services.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
